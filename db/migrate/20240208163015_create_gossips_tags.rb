2d10c787887daf2f79a523b2b6f80cd340d54b58
lexi <alexiacabanel1@gmail.com>
2024-02-08 18:42:25 +0100

    premier et dernier ajout bis, bye bye les loosers

---
+++ db/migrate/20240208163015_create_gossips_tags.rb
@@ -0,0 +1,10 @@
+class CreateGossipsTags < ActiveRecord::Migration[7.1]
+  def change
+    create_table :gossips_tags do |t|
+      t.belongs_to :gossip, index:true
+      t.belongs_to :tag, index:true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.destroy_all
+City.destroy_all
+require 'faker'
+
+cities = []
+10.times do
+  city = City.create!(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
+  cities << city
+end
+
+10.times do
+    user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence, email: Faker::Internet.email, age: Faker::Number.within(range: 18..100), city_id: cities[rand(0..9)].id)
+end
+
+20.times do
+    gossip = Gossip.create!(title: Faker::Hipster.sentence(word_count: 3, supplemental: true), content: Faker::ChuckNorris.fact, user_id: User.all.shuffle.last.id)
+end
+
+10.times do
+    tag = Tag.create!(title: Faker::Hipster.word)
+end
+
+20.times do
+    gossiptag = GossipsTag.create!(gossip_id: Gossip.all.shuffle.last.id, tag_id: Tag.all.shuffle.last.id)
+end
+
+20.times do
+    pm = PrivateMessage.create!(content: Faker::ChuckNorris.fact, recipient_id: User.all.shuffle.last.id, sender_id: User.all.shuffle.last.id)
+end
